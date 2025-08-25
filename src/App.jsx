c46241e88e6a86700353e66f6cb725b63c0675ff
SamratVsn1 <code.samzt13@gmail.com>
2025-08-25 06:45:13 +0545

    Fundamentals added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path= "" element={null}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
